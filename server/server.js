ffddf735c247487dd286da50173cf0549942d601
muharemsahin <muharem.sahinovic@live.com>
2023-07-05 15:31:55 +0200

    updates

--- server/server.js
+++ server/server.js
@@ -5,6 +5,26 @@ const db = require("./db");
 
 const app = express();
 
+var pg = require('pg');
+//or native libpq bindings
+//var pg = require('pg').native
+
+var conString = "postgres://zmuwhxyz:QadSIYyMlVS4RQg-1SR1i74Y8zI6khU3@ziggy.db.elephantsql.com/zmuwhxyz" //Can be found in the Details page
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
+  });
+});
+
 app.use(cors());
 app.use(express.json());
 
