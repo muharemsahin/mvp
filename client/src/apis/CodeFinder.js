ac91491c3836c9dc7ef3ca2723142cbd1cb15369
muharemsahin <muharem.sahinovic@live.com>
2023-08-08 17:57:38 +0200

    updates

--- client/src/apis/CodeFinder.js
+++ client/src/apis/CodeFinder.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const baseURL = process.env.NODE_ENV === "production"
  ? "/" 
- : "http://localhost:5000/"
+ : "http://localhost:3000/"
 export default axios.create({
     baseURL,
 })
